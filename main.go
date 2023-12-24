ed63031ac44c7b9f57113476c0504055ad2ad1f6
maliByatzes <malib2027@gmail.com>
2023-12-24 19:58:23 +0200

    add -b flag to toggle count bytes

--- main.go
+++ main.go
@@ -8,14 +8,18 @@ import (
 	"os"
 )
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// If the count lines flag is not set, we want to count words so we define
-	// the scanner split type to words, the default being split by lines
-	if !countLines {
+	// If the count lines flag or count bytes is not set, we want to count words
+	// so we define the scanner split type to words, the default being split by lines
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
+	}
+
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// Define the counter
@@ -34,10 +38,13 @@ func main() {
 	// Define a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
 
+	// Define a boolean flag -b to count bytes instad of words or lines
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	// Parsing the flags provided by the user
 	flag.Parse()
 
 	// Calling the count func to count the number of words
 	// received from the Standard input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
